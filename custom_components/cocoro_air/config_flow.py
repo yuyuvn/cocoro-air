5f3536b707946e1ce483cc0f3e3bdd1d403c9f23
Clicia Scarlet <yuyuvn@mac.com>
2025-02-17 01:09:41 +0900

    Fix import

--- custom_components/cocoro_air/config_flow.py
+++ custom_components/cocoro_air/config_flow.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import logging
 from typing import Any
 
+import httpx
 import voluptuous as vol
 
 from homeassistant import config_entries
@@ -75,4 +76,4 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 
 class InvalidAuth(HomeAssistantError):
-    """Error to indicate there is invalid auth.""" 
+    """Error to indicate there is invalid auth."""
