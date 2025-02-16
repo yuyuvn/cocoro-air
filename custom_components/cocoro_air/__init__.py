595e2ae05bb5203e31b50feef829f1ed889b95b7
Clicia Scarlet <yuyuvn@mac.com>
2025-02-16 23:29:15 +0900

    Support change humidity

--- custom_components/cocoro_air/__init__.py
+++ custom_components/cocoro_air/__init__.py
@@ -17,6 +17,7 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Required("email"): config_validation.string,
         vol.Required("password"): config_validation.string,
         vol.Required("device_id"): config_validation.string,
+        vol.Required("device_token"): config_validation.string,
     })
 }, extra=vol.ALLOW_EXTRA)
 
@@ -27,25 +28,28 @@ async def async_setup(hass, config):
     email = config[DOMAIN]["email"]
     password = config[DOMAIN]["password"]
     device_id = config[DOMAIN]["device_id"]
+    device_id = config[DOMAIN]["device_token"]
 
-    cocoro_air_api = CocoroAir(email, password, device_id)
+    cocoro_air_api = CocoroAir(email, password, device_id, device_token)
 
     hass.data[DOMAIN] = {
         "cocoro_air_api": cocoro_air_api,
     }
 
     await discovery.async_load_platform(hass, Platform.SENSOR, DOMAIN, {}, config)
+    await discovery.async_load_platform(hass, Platform.SWITCH, DOMAIN, {}, config)
 
     # Return boolean to indicate that initialization was successful.
     return True
 
 
 class CocoroAir:
-    def __init__(self, email, password, device_id):
+    def __init__(self, email, password, device_id, device_token):
         self.opener = httpx.Client()
         self.email = email
         self.password = password
         self.device_id = device_id
+        self.device_token = device_token
 
     def login(self):
         res = self.opener.get('https://cocoroplusapp.jp.sharp/v1/cocoro-air/login')
@@ -96,3 +100,68 @@ class CocoroAir:
             'temperature': temperature,
             'humidity': humidity,
         }
+
+    def set_humidity_mode(self, mode):
+        """Set the humidity mode of the air purifier.
+        
+        Args:
+            mode (str): The mode to set. Can be 'on' or 'off'
+        """
+        if mode not in ['on', 'off']:
+            raise ValueError("Mode must be either 'on' or 'off'")
+
+        mode_value = 'FF' if mode == 'on' else '00'
+        
+        res = self.opener.post(
+            'https://cocoroplusapp.jp.sharp/v1/cocoro-air/sync/air-cleaner',
+            json={
+                'deviceToken': self.device_token,
+                'event_key': 'echonet_control',
+                'opc': 'b0',
+                'data': [
+                    {opc: "k3", odt: {s5: "00", s7: mode_value}}
+                ]
+            }
+        )
+
+        if res.status_code == 401:
+            _LOGGER.info('Login again')
+            self.login()
+            return self.set_humidity_mode(mode)
+
+        if res.status_code != 200:
+            _LOGGER.error(f'Failed to set humidity mode, status code: {res.status_code}, response: {res.text}')
+            return False
+
+        _LOGGER.debug(f'Set humidity mode response: {res.text}')
+        return True
+
+    def get_humidity_mode(self):
+        """Get the current humidity mode status.
+        
+        Returns:
+            bool: True if humidity mode is on, False if off
+        """
+        res = self.opener.get(
+            'https://cocoroplusapp.jp.sharp/v1/cocoro-air/objects-conceal/air-cleaner',
+            params={
+                'device_id': self.device_id,
+                'event_key': 'echonet_property',
+                'opc': 'k3',
+            }
+        )
+
+        if res.status_code == 401:
+            _LOGGER.info('Login again')
+            self.login()
+            return self.get_humidity_mode()
+
+        _LOGGER.debug(f'Get humidity mode response: {res.text}')
+
+        try:
+            k3_data = res.json()['objects_aircleaner_020']['body']['data'][0]['k3']
+            mode_value = k3_data['s7']
+            return mode_value == 'FF'
+        except (KeyError, IndexError) as e:
+            _LOGGER.error(f'Failed to get humidity mode status: {e}, response: {res.text}')
+            return None
---
+++ custom_components/cocoro_air/switch.py
@@ -0,0 +1,60 @@
+"""Switch platform for Cocoro Air."""
+import logging
+from datetime import timedelta
+
+from homeassistant.components.switch import SwitchEntity
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.util import Throttle
+
+from . import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
+
+
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    """Set up the Cocoro Air Switch platform."""
+    cocoro_air_api = hass.data[DOMAIN]["cocoro_air_api"]
+    
+    add_entities([
+        CocoroAirHumiditySwitch(cocoro_air_api),
+    ])
+
+
+class CocoroAirHumiditySwitch(SwitchEntity):
+    """Representation of a Cocoro Air Humidity Switch."""
+
+    _attr_name = "Cocoro Air Humidity Mode"
+    _attr_unique_id = "cocoro_air_humidity_mode"
+    
+    def __init__(self, api):
+        """Initialize the switch."""
+        self._api = api
+        self._attr_is_on = False
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the humidity mode on."""
+        await self.hass.async_add_executor_job(self._api.set_humidity_mode, 'on')
+        self._attr_is_on = True
+        self.async_write_ha_state()
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the humidity mode off."""
+        await self.hass.async_add_executor_job(self._api.set_humidity_mode, 'off')
+        self._attr_is_on = False
+        self.async_write_ha_state()
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    async def async_update(self):
+        """Fetch new state data for the sensor."""
+        state = await self.hass.async_add_executor_job(self._api.get_humidity_mode)
+        if state is not None:
+            self._attr_is_on = state 
