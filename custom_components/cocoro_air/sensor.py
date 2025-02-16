3b0f07cf25736df65ef833611a37a5a906887194
Clicia Scarlet <yuyuvn@mac.com>
2025-02-17 00:02:30 +0900

    Update to new syntax

--- custom_components/cocoro_air/sensor.py
+++ custom_components/cocoro_air/sensor.py
@@ -1,127 +1,72 @@
-"""Platform for sensor integration."""
+"""Sensor platform for Cocoro Air."""
+from __future__ import annotations
 
-import logging
-
-from datetime import timedelta
-
-from homeassistant.core import callback
 from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
     SensorStateClass,
 )
-from homeassistant.const import UnitOfTemperature
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import PERCENTAGE, UnitOfTemperature
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, CoordinatorEntity
 
-from . import DOMAIN, CocoroAir
-
-_LOGGER = logging.getLogger(__name__)
+from . import DOMAIN
 
 
-async def async_setup_platform(
-        hass: HomeAssistant,
-        _config: ConfigType,
-        async_add_entities: AddEntitiesCallback,
-        discovery_info: DiscoveryInfoType | None = None
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
 ) -> None:
-    """Set up the sensor platform."""
-    if discovery_info is None:
-        return
+    """Set up Cocoro Air sensor platform."""
+    cocoro_air_api = hass.data[DOMAIN][entry.entry_id]["cocoro_air_api"]
 
-    _LOGGER.debug("Setting up sensor platform.")
-
-    cocoro_air_api = hass.data[DOMAIN]["cocoro_air_api"]
-    coordinator = MyCoordinator(hass, cocoro_air_api)
-
-    async_add_entities([
-        CocoroAirTemperatureSensor(coordinator),
-        CocoroAirHumiditySensor(coordinator),
-    ])
+    async_add_entities(
+        [
+            CocoroAirTemperatureSensor(cocoro_air_api),
+            CocoroAirHumiditySensor(cocoro_air_api),
+        ]
+    )
 
 
-class MyCoordinator(DataUpdateCoordinator):
-    """My custom coordinator."""
+class CocoroAirTemperatureSensor(SensorEntity):
+    """Representation of a Cocoro Air Temperature Sensor."""
 
-    def __init__(self, hass, my_api: CocoroAir):
-        """Initialize my coordinator."""
-        super().__init__(
-            hass,
-            _LOGGER,
-            # Name of the data. For logging purposes.
-            name="Cocoro Air update",
-            # Polling interval. Will only be polled if there are subscribers.
-            update_interval=timedelta(seconds=60),
-        )
-        self.my_api = my_api
+    _attr_device_class = SensorDeviceClass.TEMPERATURE
+    _attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
+    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_name = "Cocoro Air Temperature"
+    _attr_unique_id = "cocoro_air_temperature"
 
-    async def _async_update_data(self):
-        """Fetch data from API endpoint.
-
-        This is the place to pre-process the data to lookup tables
-        so entities can quickly look up their data.
-        """
-        return self.my_api.get_sensor_data()
-
-
-class CocoroAirSensorBase(CoordinatorEntity, SensorEntity):
-    """Base class for Cocoro Air sensor."""
-
-    def __init__(self, coordinator: DataUpdateCoordinator, name: str,
-                 device_class: SensorDeviceClass, state_class: str, unit_of_measurement: str):
+    def __init__(self, api):
         """Initialize the sensor."""
-        super().__init__(coordinator)
-        self._attr_name = name
-        self._attr_unique_id = f"{DOMAIN}_{name.lower()}"
-        self._attr_device_class = device_class
-        self._attr_state_class = state_class
-        self._attr_native_unit_of_measurement = unit_of_measurement
+        self._api = api
+        self._attr_native_value = None
 
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        """Handle updated data from the coordinator."""
-        sensor_data = self.coordinator.data
-        self._attr_native_value = sensor_data[self._attr_name.lower()]
-        self.async_write_ha_state()
-
-    def device_info(self) -> DeviceInfo | None:
-        """Return device information about this entity."""
-        return {
-            "identifiers": {(DOMAIN, self.coordinator.my_api.device_id)},
-            "name": "Cocoro Air",
-            "manufacturer": "Sharp",
-            "model": "Cocoro Air",
-        }
+    async def async_update(self) -> None:
+        """Fetch new state data for the sensor."""
+        data = await self.hass.async_add_executor_job(self._api.get_sensor_data)
+        if data:
+            self._attr_native_value = data["temperature"]
 
 
-class CocoroAirTemperatureSensor(CocoroAirSensorBase):
-    """Cocoro Air temperature sensor."""
+class CocoroAirHumiditySensor(SensorEntity):
+    """Representation of a Cocoro Air Humidity Sensor."""
 
-    def __init__(self, coordinator: DataUpdateCoordinator):
+    _attr_device_class = SensorDeviceClass.HUMIDITY
+    _attr_native_unit_of_measurement = PERCENTAGE
+    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_name = "Cocoro Air Humidity"
+    _attr_unique_id = "cocoro_air_humidity"
+
+    def __init__(self, api):
         """Initialize the sensor."""
-        super().__init__(
-            coordinator,
-            "Temperature",
-            SensorDeviceClass.TEMPERATURE,
-            SensorStateClass.MEASUREMENT,
-            UnitOfTemperature.CELSIUS
-        )
-        self._attr_icon = "mdi:thermometer"
+        self._api = api
+        self._attr_native_value = None
 
-
-class CocoroAirHumiditySensor(CocoroAirSensorBase):
-    """Cocoro Air humidity sensor."""
-
-    def __init__(self, coordinator: DataUpdateCoordinator):
-        """Initialize the sensor."""
-        super().__init__(
-            coordinator,
-            "Humidity",
-            SensorDeviceClass.HUMIDITY,
-            SensorStateClass.MEASUREMENT,
-            "%"
-        )
-        self._attr_icon = "mdi:water-percent"
+    async def async_update(self) -> None:
+        """Fetch new state data for the sensor."""
+        data = await self.hass.async_add_executor_job(self._api.get_sensor_data)
+        if data:
+            self._attr_native_value = data["humidity"]
